63476bfd351c511af8dd75414f1d84b9027ea7fc
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2025-05-04 22:01:12 -0300

    Semana 1 - Aula 9

---
+++ Classroom/Semana_1.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+void main()
+{
+
+    // Assignments
+    int a, b, c;
+    float f = 2.45;
+
+    a = f;  // a == INT, the decimals are not stored
+    a = b = 30; // multiple assignment
+    printf("%i\n", a);
+
+    // INPUT
+        // scanf("", &d) - data type(lecture format), address of data
+
+    scanf("%i", &c);
+    printf("%i\n", c);
+
+    // Arithmetic operators
+
+
+}
--- README.md
+++ README.md
@@ -4,7 +4,17 @@
 - Códigos relacionados a Disciplina de IP1
 - **Linguagem utilizada**: C
 - **IDE**: Replit e CLion
-  
+
+---
+### Aulas
+
+- [Aula 1](DC/Aula_1.c) - Assignments and I/O
+- 
+
+### Curso
+
+- [Semana 1](Classroom/Semana_1.c)
+
 ### PROJETOS
 
 1.
