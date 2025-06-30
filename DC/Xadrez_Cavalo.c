8bbf46ed1221e96ea2b6e968f2249ac171cceeca
Gabriel Belo <gabrielbelo.dev@gmail.com>
2025-06-30 15:54:32 -0300

    Create Xadrez_Cavalo.c

---
+++ DC/Xadrez_Cavalo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(void) {
+    srand(time(NULL));
+    int tam = 8;
+    char mapa[tam][tam+1];
+
+    for(int i = 0; i < tam; i++){
+        for(int j= 0; j < tam; j++){
+            mapa[i][j] = '.';
+        }
+    }
+
+    int l_c = rand() % tam;
+    int c_c = rand() % tam;
+    mapa[l_c][c_c] = 'C';
+
+    for(int qtd_pecas = 0; qtd_pecas < 8; qtd_pecas++){
+        int l_p = rand() % 8;
+        int c_p = rand() % 8;
+
+        if (mapa[l_p][c_p] == '.'){
+            mapa[l_p][c_p] = 'I';
+        } else {
+            qtd_pecas--;
+        }
+    }
+
+    for(int i = 0; i < tam; i++){
+        for(int j= 0; j < tam; j++){
+            printf("%c ", mapa[i][j]);
+        }
+        printf("\n");
+    }
+
+    int comeu;
+    //Pode se mover para cima
+    if(l_c - 2 >= 0){
+        // Cima - Direita
+        if(c_c + 1 < tam){
+            if(mapa[l_c - 2][c_c + 1] == 'I'){
+                comeu++;
+            }
+        }
+        // Cima - Esquerda    
+        else if(c_c - 1 > 0){
+            if(mapa[l_c - 2][c_c - 1] == 'I') 
+            {
+                 comeu++;  
+            }
+        }
+    }
+
+    printf("%i", comeu);
+    
+    // // Pode se mover para baixo
+    // else if(l_c - 2 <= tam){
+    //     // Mover para direita
+    //     if(mapa[l_c - 2][c_c + 1] == '.'){
+    //         comeu++;
+    //     }
+    // }
+    
+}
