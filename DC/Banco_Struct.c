2fb77e3a7f5ceab51b6e7e404d4b9b28757bfbfd
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2025-07-03 21:51:28 -0300

    Finish - Consulta no Banco

--- DC/Banco_Struct.c
+++ DC/Banco_Struct.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <string.h>
 
 typedef struct
@@ -11,11 +9,12 @@ typedef struct
 
 void main(void) {
 
-    int qtd_contas = 1;
+    int qtd_contas = 3;
     Conta contas[qtd_contas];
-    
+
+    printf("***Banco do Brasil***\n\n");
     for(int i = 0; i < qtd_contas; i++){
-        printf("Registre sua conta:\n\n");
+        printf("***Registre sua conta***\n");
         char numero_digitado[2];
         float saldo_solicitado;
 
@@ -27,20 +26,20 @@ void main(void) {
         scanf("%f", &contas[i].saldo);
         getchar();
 
-        printf("Conta %s registrada com sucesso!\n\n");
+        printf("Conta %s registrada com sucesso!\n\n",  contas[i].numero);
     }
-    
+
     while(1){
-        
+
         int existe = 0;
         char acessar_conta[3];
-        printf("\nQual conta você deseja acessar: (Listar as contas, digite 'v')\n");
+        printf("\nQual conta você deseja acessar: ");
         scanf("%s", acessar_conta);
         getchar();
 
         char numero_conta[3];
-        float saldo_conta;
-        int registro_conta = 0;
+        float saldo_conta = 0;
+        int registro_conta = -1;
         
         for(int i = 0; i < qtd_contas; i++){
             if(strcmp(contas[i].numero, acessar_conta) == 0){
@@ -51,13 +50,16 @@ void main(void) {
             }
         }
 
-        if(registro_conta == 0){
-            printf("Essa conta não está registrada!\n");
+        if(registro_conta == -1){
+            printf("Essa conta não está registrada!\nContas registradas:\n");
+            for(int i = 0; i < qtd_contas; i++){
+                printf("%s\n", contas[i].numero);
+            }
             continue;
         }
 
         char operacao;
-        printf("\n\nQual operação você deseja: Saldo (s) | Creditar (c) | Debitar (d) | Ver contas (v) : ");
+        printf("\nQual operação você deseja: Saldo (s) | Creditar (c) | Debitar (d): ");
         scanf("%c", &operacao);
         getchar();
 
@@ -65,15 +67,8 @@ void main(void) {
         
         switch(operacao)
         {
-            case 'v':
-                for(int i = 0; i < qtd_contas; i++){
-                    printf("%s\n", contas[i].numero);
-                    printf("%.2f\n\n", contas[i].saldo);        
-                }
-                break;
-
             case 's':
-                printf("O saldo da sua conta é: %f", saldo_conta);
+                printf("O saldo da sua conta é: %.2f\n\n", saldo_conta);
                 break;
 
             case 'c':
@@ -82,19 +77,26 @@ void main(void) {
                 getchar();
 
                 contas[registro_conta].saldo += valor;
+                printf("Seu saldo atualizado: %.2f\n\n", contas[registro_conta].saldo);
+                break;
 
             case 'd':
                 printf("Digite o valor a ser debitado: ");
                 scanf("%f", &valor);
                 getchar();
+
+                contas[registro_conta].saldo -= valor;
+                printf("Seu saldo atualizado:  %.2f\n\n", contas[registro_conta].saldo);
+                break;
         }
 
         char continuar;
-        printf("Deseja fazer outra operação? (s)(n)");
+        printf("Deseja fazer outra operação (s)(n)? ");
         scanf("%c", &continuar);
         getchar();
 
         if (continuar == 'n'){
+            printf("\n***Banco do Brasil***");
             break;
         }
     }
