f1b378749cdedce434201ba2b86debabd789284c
gabrielbelo2007 <gabrielbelo.dev@gmail.com>
2025-11-14 00:05:59 -0300

    Lista 5 - Questão E

--- CIn/Lista_5/fuga.py
+++ CIn/Lista_5/fuga.py
@@ -5,11 +5,20 @@ n = int(input())
 ala_ocupada = int(input())
 lote_ocupado = int(input())
 
-tabuleiro = []
+while (ala_ocupada > n or lote_ocupado > n) or (ala_ocupada <= 0 or lote_ocupado <= 0):
+    print(f"Rogério e Chaguinha não encontraram o túmulo ocupado na posição {ala_ocupada, lote_ocupado}. Assim eles nunca vão conseguir sair do cemitério!")
 
+    ala_ocupada = int(input())
+    lote_ocupado = int(input())
+
+print(f"Rogério e Chaguinha conseguiram encontrar o túmulo ocupado em {ala_ocupada, lote_ocupado}!\n")
+
+# Construção tabuleiro
+tabuleiro = []
 for ala in range(n):
-    
-    if ala != ala_ocupada:
+
+    # ala_ocupada e lote_ocupado subtraído 1 = index
+    if ala != (ala_ocupada - 1):
         tabuleiro.append([0 for lotes in range(n)])
     
     else:
@@ -17,7 +26,7 @@ for ala in range(n):
 
         for lote in range(n):
 
-            if lote == lote_ocupado:
+            if lote == (lote_ocupado - 1):
                 ala_selada.append(2)
 
             else:
@@ -29,51 +38,64 @@ for ala in range(n):
 def sem_ameaca(tabuleiro, linha, coluna):
 
     tam = len(tabuleiro)
-
-    # Verifica se tem uma rainha na ala(linha)
-    # if 1 in tabuleiro[posicao[0]]:
-    #    return False
-
+    
     # Verificar posição selada
     if tabuleiro[linha][coluna] == 2:
         return False
     
-    
-    for ala in range(tam):
-
-        # Verifica se tem uma rainha no lote(coluna)
-        if 1 == tabuleiro[ala][coluna]:
+    # Verifica todas as linhas até a última linha que teve uma rainha posicionada
+    for ala_1 in range(linha):
+        
+        if 1 == tabuleiro[ala_1][coluna]:
             return False
         
-        if 1 in tabuleiro[ala]:
+        lote_1 = tabuleiro[ala_1].index(1)
 
-            ala_1 = ala
-            lote_1 = tabuleiro[ala].index(1)
+        ala_2 = linha # ala_atual
+        lote_2 = coluna # lote_atual
 
-            ala_2 = linha # ala
-            lote_2 = coluna # lote
+        if abs(ala_1 - ala_2) == abs(lote_1 - lote_2):
+            return False
 
-            if abs(ala_1 - ala_2) == abs(lote_1 - lote_2):
-                return False
-            
+    # Se posição livre         
     return True
         
 
 # 0 = Casa Livre | 1 = Rainha | 2 = Selo
-def posicoes_possiveis(tabuleiro, ala, lote=0):
+def posicoes_possiveis(tabuleiro, ala=0):
     
-    borda = len(tabuleiro)
+    tam = len(tabuleiro)
+    combinacoes = 0
 
-    # Caso base (talvez errado)
-    # if posicao[0] == borda:
-    #     return 1
+    # Caso base
+    if ala == tam:
+        return 1
     
-    while not sem_ameaca(tabuleiro, ala, lote):
-        lote += 1 
+    for lote in range(tam):
 
-        if lote == len(tabuleiro):
-            return 0
+        if sem_ameaca(tabuleiro, ala, lote):
 
-    tabuleiro[ala][lote] = 1
+            tabuleiro[ala][lote] = 1
 
-    posicoes_possiveis(tabuleiro, ala + 1)
+            combinacoes += posicoes_possiveis(tabuleiro, ala + 1)
+
+            tabuleiro[ala][lote] = 0
+
+    return combinacoes
+
+possibilidades = posicoes_possiveis(tabuleiro)
+
+print(f"Rogério e Chaguinha conseguiram encontrar {possibilidades} possíveis posições para as almas se posicionarem sem conflitos!")
+
+if possibilidades == 0:
+    print(f"Não existe nenhuma configuração segura para as almas... Rogério e Chaguinha estão presos no meio da guerra das almas!")
+
+else:
+    if possibilidades <= 10:
+        print("Os amigos vão precisar tomar muito cuidado para não pegar um caminho errado!")
+
+    elif 50 >= possibilidades > 10:
+        print("Uau! São tantas opções que eles até se perderam contando!")
+    
+    else:
+        print("Em pleno Halloween e as almas descansando em paz! Rogério e Chaguinha vão conseguir sair logo do cemitério.")
---
+++ CIn/Lista_5/labirinto.py
@@ -0,0 +1,116 @@
+horario = input()
+minutos_restantes = 60 - int(horario[3:])
+
+print(f"O relógio marca 23 horas e {horario[3:]} minuto(s)! Byte tem apenas {minutos_restantes} minuto(s) para escapar!")
+
+tam = int(input())
+
+def caminho_livre(linha, coluna):
+
+    if [linha, coluna] in aboboras:
+        return "abóbora"
+    
+    if (linha == tam or coluna == tam) or (linha < 0 or coluna < 0):
+        return "bloqueado"
+    
+    return "livre"
+
+# Lista de memória do menor caminho para os pontos do labirinto
+tempo_caminhos = [[minutos_restantes + 1 for coluna in range(tam)] for linha in range(tam)]
+
+menor_caminho = [minutos_restantes + 1]
+
+def calcular_caminho(byte, ult_movimento=-1, minutos=0):
+    
+    # Caso base
+    if byte == saida:
+
+        if minutos < menor_caminho[0]:
+            menor_caminho[0] = minutos
+        return  1
+    
+    if minutos >= menor_caminho[0]:
+        return 0
+    
+    if minutos == minutos_restantes:
+        return 0
+
+    # Os 4 movimentos possíveis são todas as direções
+    for movimento in range(4):
+
+        mudanca_x = 0
+        mudanca_y = 0
+
+        # Para cima
+        if movimento == 0 and ult_movimento != 1:
+            mudanca_x = -1
+
+        # Para baixo
+        elif movimento == 1 and ult_movimento != 0:
+            mudanca_x = 1
+        
+        # Para direita
+        elif movimento == 2 and ult_movimento != 3:
+            mudanca_y = 1
+        
+        # Para esquerda
+        elif movimento == 3 and ult_movimento != 2:
+            mudanca_y = -1
+
+        byte[0] += mudanca_x
+        byte[1] += mudanca_y
+
+        resultado = caminho_livre(byte[0], byte[1])
+
+        if resultado != "abóbora" and resultado != "bloqueado":
+
+            if minutos + 1 < tempo_caminhos[byte[0]][byte[1]]:
+                tempo_caminhos[byte[0]][byte[1]] = minutos + 1
+                calcular_caminho(byte, movimento, minutos + 1)
+
+        byte[0] -= mudanca_x
+        byte[1] -= mudanca_y
+    
+    return 0
+
+labirinto = []
+
+# Posições do labirinto
+aboboras = []
+saida = []
+byte = []
+
+for linhas in range(tam):
+    linha = list(input())
+
+    for coluna in range(tam):
+
+        if linha[coluna] == "A":
+            aboboras.append([linhas, coluna])
+        
+        elif linha[coluna] == "B":
+            byte += linhas, coluna
+        
+        elif linha[coluna] == "S":
+            saida += linhas, coluna
+
+    labirinto.append(linha)
+
+# Definindo posição inicial = 0 para evitar ciclos
+tempo_caminhos[byte[0]][byte[1]] = 0
+
+calcular_caminho(byte)
+
+# Não escapou (menor caminho não foi alterado)
+if menor_caminho[0] == minutos_restantes + 1:
+    print("NÃÃÃÃO! Tudo isso por causa de um docinho! Você estará para sempre conosco, Byte!")
+
+else:
+
+    print(f"CONSEGUIMOS!! Byte precisou de {menor_caminho[0]} minuto(s) para conseguir escapar!")
+
+    if menor_caminho[0] < (minutos_restantes - 10):
+        print(f"Abóboras CInistras que nada! Byte mostrou quem é que manda e conseguiu sair faltando {minutos_restantes - menor_caminho[0]} minutos para elas acordarem")
+
+    else:
+        print("Ufa! Essa foi por pouco! Mas com ajuda dos alunos de IP essas abóboras nem pareciam tão sinistras assim.")
